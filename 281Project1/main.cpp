b1621d85a79621f6b14d3bb0448adedd8bad762c
dylanbp <dylanphillips@0587393948.wireless.umich.net>
2015-01-19 13:44:49 -0500

    Initial Commit

---
+++ 281Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  281Project1
+//
+//  Created by dylanbp on 1/19/15.
+//  Copyright (c) 2015 Dylan Phillips. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
